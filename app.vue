077667c2f684e6b0b4e1d612ccf73c00b5b65b76
Adam Podhorsky <apodhors@gmail.com>
2025-02-09 10:26:32 +0100

    Refactor app layout and add default page structure

--- app.vue
+++ app.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <h1 class="text-3xl font-bold underline">
-      Nuxt Minimal Starter Template with Tailwind CSS, ESlint and Prettier
-    </h1>
-  </div>
+  <NuxtRouteAnnouncer />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
