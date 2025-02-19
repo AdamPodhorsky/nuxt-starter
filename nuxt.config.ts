678587ce81cd73ac4872eeb4d8bca7ac8d9b434e
Adam Podhorsky <apodhors@gmail.com>
2025-02-19 23:54:25 +0100

    Bump tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,10 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
-  css: ['~/assets/css/tailwind.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  css: ['~/assets/css/main.css'],
+  vite: { plugins: [tailwindcss()] },
 })
